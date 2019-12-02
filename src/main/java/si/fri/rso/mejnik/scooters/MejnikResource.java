8fe90dea28b2c0e247866d8fb658fe643316367d
ghajduk3 <hajdukovicgojko3@gmail.com>
2019-12-02 16:35:57 +0100

    travis

--- src/main/java/si/fri/rso/mejnik/scooters/MejnikResource.java
+++ src/main/java/si/fri/rso/mejnik/scooters/MejnikResource.java
@@ -14,11 +14,6 @@ public class MejnikResource {
     public Response getMejnik() {
         return Response.status(Response.Status.OK).entity("{\n" +
                 "    \"clani\": [\"gh8590\"],\n" +
-                "    \"opis_projekta\": \"Nas projekt implementira aplikacijo za oddajo elektricnih skutera.\",\n" +
-                "    \"mikrostoritve\": [\"http://35.189.96.118:8081/v1/orders\", \"http://35.197.209.159:8080/v1/customers/\"],\n" +
-                "    \"github\": [\"https://github.com/jmezna/rso-customers\", \"https://github.com/jmezna/rso-orders\"],\n" +
-                "    \"travis\": [\"https://travis-ci.org/jmezna/rso-customers\", \"https://travis-ci.org/jmezna/rso-orders\"],\n" +
-                "    \"dockerhub\": [\"https://hub.docker.com/r/jmezna/rso-customers/\", \"https://hub.docker.com/r/jmezna/rso-orders/\"]\n" +
-                "}").build();
+                "    \"opis_projekta\": \"This service is just an API to pass mejnik\",\n"}").build();
     }
 }
